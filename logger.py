004c5a20f7d9e1044ffc86315f0024f830ad096e
Syed Affan Hussain <syedaffan.dev@gmail.com>
2025-05-22 19:21:25 +0200

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,13 +1,21 @@
 import logging
+from logging.handlers import RotatingFileHandler
 
-def setup_logger(name: str) -> logging.Logger:
-    logger = logging.getLogger(name)
-    logger.setLevel(logging.INFO)
+def setup_logger(log_file='app.log', log_level=logging.INFO):
+    logger = logging.getLogger('application_logger')
+    logger.setLevel(log_level)
 
-    if not logger.handlers:
-        ch = logging.StreamHandler()
-        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-        ch.setFormatter(formatter)
-        logger.addHandler(ch)
+    # Create a file handler for rotating logs
+    handler = RotatingFileHandler(
+        log_file, maxBytes=5 * 1024 * 1024, backupCount=5
+    )
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    )
+    handler.setFormatter(formatter)
+
+    # Add the handler to the logger
+    if not logger.hasHandlers():
+        logger.addHandler(handler)
 
     return logger
